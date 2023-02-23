9cb33205b638615b800cb324016549d7654de2fd
Emil Kryukov <77118598+mcrewcow@users.noreply.github.com>
2023-02-23 00:04:43 -0500

    notes for the relevant code

---
+++ processing.R
@@ -0,0 +1,3 @@
+#Please check what figures were added to the paper and choose the relevant analysis for it
+#If i remember right, we did basic processing, cytotrace, cell cycle analysis and cellchat
+#Cellchat should be added 100%
