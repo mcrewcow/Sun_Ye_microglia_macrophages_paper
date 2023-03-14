d3366c3a6d6a1ccf12213813efb61bf0d8e2a385
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-03-14 14:47:16 -0400

    Update cellchat.R

--- cellchat.R
+++ cellchat.R
@@ -1,15 +1,15 @@
-sunye <- readRDS("G://Sun_Ye/Processed/P13_0.5_3.RDS")
-sunye_normal <- subset(sunye, subset = group == 'Normal')
+sunye <- readRDS("G://Sun_Ye/Processed/P13_0.5_3.RDS") #read the preprocessed file
+sunye_normal <- subset(sunye, subset = group == 'Normal') #subset by conditions
 head(sunye_normal)
 DimPlot(sunye_normal, label = TRUE, label.box = TRUE)
 sunye_normal$labels <- sunye_normal@active.ident
 sunye_OIRKO <- subset(sunye, subset = group == 'OIR_KO')
 sunye_OIRKO$labels <- sunye_OIRKO@active.ident
 sunye_OIRWT <- subset(sunye, subset = group == 'OIR_WT')
-sunye_OIRWT$labels <- sunye_OIRWT@active.ident
+sunye_OIRWT$labels <- sunye_OIRWT@active.ident #save labels in a separated column
 
-library(CellChat)
-cellchat <- createCellChat(object = sunye_normal, group.by = "labels")
+library(CellChat) #perform CellChat analysis
+cellchat <- createCellChat(object = sunye_normal, group.by = "labels") #grouped by cell populations
 CellChatDB <- CellChatDB.mouse
 CellChatDB.use <- CellChatDB
 cellchat@DB <- CellChatDB.use
@@ -17,9 +17,9 @@ cellchat <- subsetData(cellchat)
 cellchat <- identifyOverExpressedGenes(cellchat)
 cellchat <- identifyOverExpressedInteractions(cellchat)
 cellchat <- projectData(cellchat, PPI.human)
-cellchat <- computeCommunProb(cellchat, type = "truncatedMean", trim = 0.1, raw.use = FALSE, population.size = TRUE)
-cellchat <- filterCommunication(cellchat, min.cells = 3)
+cellchat <- computeCommunProb(cellchat, type = "truncatedMean", trim = 0.1, raw.use = FALSE, population.size = TRUE) 
+cellchat <- filterCommunication(cellchat, min.cells = 3) 
 cellchat <- computeCommunProbPathway(cellchat)
 cellchat <- aggregateNet(cellchat)
 cellchat <- netAnalysis_computeCentrality(cellchat, slot.name = "netP")
-netVisual_aggregate(cellchat, signaling = c('SPP1'), layout = "chord")
+netVisual_aggregate(cellchat, signaling = c('SPP1'), layout = "chord") #SPP1 as example, can dig into L-R pairs, contribution and specific L-R pair chord diagram
